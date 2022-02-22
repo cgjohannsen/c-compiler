7ceef888b00ddc9119bf1cbb5412079cd6bca174
Chris <chrisjohannsen8@gmail.com>
2022-02-21 19:36:12 -0600

    removing comments

--- Source/util/io.c
+++ Source/util/io.c
@@ -67,48 +67,6 @@ open_file(char *filename)
     }
 
     return fp;
-
-    /*
-    FILE *fp;
-    long file_size;
-    char *buffer;
-    size_t bytes_read;
-
-    fp = fopen(filename, "r");
-    if(fp == NULL) {
-        print_msg(FILE_ERR, filename, 0, 0, "Could not open file");
-        return NULL;
-    }
-
-    // get file size
-    fseek (fp, 0, SEEK_END);
-    file_size = ftell(fp);
-    rewind(fp);
-
-    if(file_size > MAX_FILE_SIZE) {
-        print_msg(FILE_ERR, filename, 0, 0, "File size too big");
-        return NULL;
-    }
-
-    // allocate memory to contain the whole file + '\0' at end of buffer    buffer = (char*) malloc((sizeof(char)*file_size)+1);
-    if (buffer == NULL) {
-        print_msg(FILE_ERR, filename, 0, 0, "Could not allocate memory for file");
-        return NULL;
-    }
-
-    // copy the file into the buffer
-    bytes_read = fread(buffer, 1, file_size, fp);
-    if (bytes_read != file_size) {
-        print_msg(FILE_ERR, filename, 0, 0, "Could not read entire file contents");
-        return NULL;
-    }
-
-    // set last char of buffer to 0
-    buffer[file_size] = 0;
-
-    fclose(fp);
-    return buffer;
-    */
 }
 
 /*
